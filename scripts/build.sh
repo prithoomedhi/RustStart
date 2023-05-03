0ed4846208d8c0f54dde4107b7bbc27d8080e993
prithoomedhi <prithoo.medhi@mslate.ai>
2023-05-04 01:37:32 +0530

    build and run scripts.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,18 @@
-# $_type = "$arg:-release}"
+## Usage: sh scrpts/build.sh --arg=release
 
-echo "arg = $arg"
+arg="$1"
 
-if [$arg=="release"]
-then
+if [[ "$arg" == "release" ]]; then
     echo "Building release version..."
     cargo build --release
     echo "...build complete."
-elif [$arg=="debug"]
-then
+elif [[ "$arg" == "debug" ]]; then
     echo "Building debug version..."
-    cargo build --debug
+    cargo build
+    echo "...build complete."
+else
+    echo "Unknown build type: $arg; building debug version..."
+    cargo build
     echo "...build complete."
 fi
+
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,17 @@
-cargo run src/main.rs
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo "OS: $OSTYPE"
+        ./target/release/rust_start
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "OS: Mac OSX ($OSTYPE)"
+        ./target/release/rust_start
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+        echo "OS: Cygwin ($OSTYPE)"
+        ./target/release/rust_start.exe
+elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+        echo "OS: Windows-MinGW ($OSTYPE)"
+        ./target/release/rust_start.exe
+else
+        echo "Unknown OS type: $OSTYPE"
+fi
