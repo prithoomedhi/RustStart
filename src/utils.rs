f82f96a42ce03b46263957e6e791c4b02a5d558d
prithoomedhi <prithoo.medhi@mslate.ai>
2023-05-04 02:57:11 +0530

    documentation of self-written code.

--- src/utils.rs
+++ src/utils.rs
@@ -4,6 +4,19 @@ General purpose utility functions.
 
 
 pub fn unique_elements_vector<T: std::ops::Add<Output = T> + std::fmt::Debug + std::cmp::PartialEq>(_list: Vec<T>)->Vec<T>{
+    /*
+    Find all the UNIQUE elements in a given vector of Datatype T*
+        * where T has the following attributes:
+            Add()
+            Debug()
+            PartialEq()
+
+    Arguments:
+        _list: Vec<T>
+    
+    Returns:
+        Vec<T>
+    */
     let mut unique_list:Vec<T> = Vec::new();
     for item in _list{
         if !unique_list.contains(&item){
