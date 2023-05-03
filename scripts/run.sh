a11c3ac566099147037fa265afb8e26222983021
prithoomedhi <prithoo.medhi@mslate.ai>
2023-05-04 02:41:18 +0530

    corrections in run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,50 +1,69 @@
-$arg = "$1"
+arg="$1"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         echo "OS: $OSTYPE"
         
-        if [$arg -eq "release"]; then
+        if [[ "$arg" == "release" ]]; then
                 ./target/release/rust_start
-        elif [$arg -eq "debug"]; then
+        elif [[ "$arg" == "debug" ]]; then
                 ./target/debug/rust_start
         else
-                echo "Unknown build type: $arg; building debug version..."
-                ./target/debug/rust_start
+                if [[ "$arg" == "" ]]; then
+                        arg="NULL"
+                elif [ -z "$arg" ]; then
+                        arg="NULL"
+                fi
+                echo "Unknown build type:    $arg"
         fi
+        
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "OS: Mac OSX ($OSTYPE)"
         
-        if [$arg -eq "release"]; then
+        if [[ "$arg" == "release" ]]; then
                 ./target/release/rust_start
-        elif [$arg -eq "debug"]; then
+        elif [[ "$arg" == "debug" ]]; then
                 ./target/debug/rust_start
         else
-                echo "Unknown build type: $arg; building debug version..."
-                ./target/debug/rust_start
+                if [[ "$arg" == "" ]]; then
+                        arg="NULL"
+                elif [ -z "$arg" ]; then
+                        arg="NULL"
+                fi
+                echo "Unknown build type:    $arg"
         fi
+        
 elif [[ "$OSTYPE" == "cygwin" ]]; then
         # POSIX compatibility layer and Linux environment emulation for Windows
         echo "OS: Cygwin ($OSTYPE)"
         
-        if [$arg -eq "release"]; then
+        if [[ "$arg" == "release" ]]; then
                 ./target/release/rust_start.exe
-        elif [$arg -eq "debug"]; then
+        elif [[ "$arg" == "debug" ]]; then
                 ./target/debug/rust_start.exe
         else
-                echo "Unknown build type: $arg; building debug version..."
-                ./target/debug/rust_start.exe
+                if [[ "$arg" == "" ]]; then
+                        arg="NULL"
+                elif [ -z "$arg" ]; then
+                        arg="NULL"
+                fi
+                echo "Unknown build type:    $arg"
         fi
+
 elif [[ "$OSTYPE" == "msys" ]]; then
         # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
         echo "OS: Windows-MinGW ($OSTYPE)"
 
-        if [$arg -eq "release"]; then
+        if [[ "$arg" == "release" ]]; then
                 ./target/release/rust_start.exe
-        elif [$arg -eq "debug"]; then
+        elif [[ "$arg" == "debug" ]]; then
                 ./target/debug/rust_start.exe
         else
-                echo "Unknown build type: $arg; building debug version..."
-                ./target/debug/rust_start.exe
+                if [[ "$arg" == "" ]]; then
+                        arg="NULL"
+                elif [ -z "$arg" ]; then
+                        arg="NULL"
+                fi
+                echo "Unknown build type:    $arg"
         fi
 else
         echo "Unknown OS type: $OSTYPE"
